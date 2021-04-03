900c420d3297cb603efa538434436017a50a4d1b
chrispope12391 <popex107@umn.edu>
2021-04-02 19:23:22 -0500

    12

--- Mission_to_Mars/scrape_mars.py
+++ Mission_to_Mars/scrape_mars.py
@@ -4,6 +4,8 @@ import time
 from webdriver_manager.chrome import ChromeDriverManager
 
 def scrape():
+
+    ##This will the 
     # browser = init_browser()
     executable_path = {'executable_path': ChromeDriverManager().install()}
     browser = Browser('chrome', **executable_path, headless=False)
@@ -19,12 +21,21 @@ def scrape():
     soup = BeautifulSoup(html, 'html.parser')
 
     # Retrieve all elements that contain mars article information
-    mars_results = soup.find_all('div', class_='content_title')
+    # mars_title = soup.find_all('div', class_='content_title')
+    # mars_teaser = soup.find('div', class_='article_teaser_body').text
 
     mars_scrape = {}
-    mars_teaser = []
 
     mars_scrape['title'] = soup.find('div', class_='content_title').text
+    mars_scrape['teaser'] = soup.find('div', class_='article_teaser_body').text
     browser.quit()
     
+    # This will pull the featured image
+    new_browser = Browser('chrome', **executable_path, headless=False)
+
+    image_url = "https://spaceimages-mars.com/"
+    new_browser.visit(image_url)
+
+    new_browser.quit()
+
     return mars_scrape
