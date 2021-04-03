ff9641487b8c141bef08053ad3348c687f0a3660
chrispope12391 <popex107@umn.edu>
2021-04-02 23:48:36 -0500

    14

--- Mission_to_Mars/scrape_mars.py
+++ Mission_to_Mars/scrape_mars.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from splinter import Browser
 from bs4 import BeautifulSoup
 import time
@@ -47,5 +48,86 @@ def scrape():
     mars_scrape['feautred_img_url'] = image_url+image_results
 
     new_browser.quit()
+
+    # facts_url = "https://galaxyfacts-mars.com/"
+
+    # tables = pd.read_html(facts_url)
+
+    # facts_df = tables[1]
+
+    # mars_scrape = mars_scrape.insert_many(tables)
+
+    hemi_url = "https://marshemispheres.com/"
+    valles_url = hemi_url +"valles.html"
+    cerbrus_url = hemi_url +"cerberus.html"
+    schia_url = hemi_url +"schiaparelli.html"
+    syrt_url = hemi_url +"syrtis.html"
+
+    #create browsers for each hemisphere
+    valles_browser = Browser('chrome', **executable_path, headless=False)
+    cerb_browser = Browser('chrome', **executable_path, headless=False)
+    schia_browser = Browser('chrome', **executable_path, headless=False)
+    syrt_browser = Browser('chrome', **executable_path, headless=False)
+
+
+    valles_browser.visit(valles_url)
+    cerb_browser.visit(cerbrus_url)
+    schia_browser.visit(schia_url)
+    syrt_browser.visit(syrt_url)
+
+    # HTML object
+    valles_html = valles_browser.html
+    cerb_html = cerb_browser.html
+    schia_html = schia_browser.html
+    syrt_html = syrt_browser.html
+
+    # Parse HTML with Beautiful Soup
+    valles_soup = BeautifulSoup(valles_html, 'html.parser')
+    cerb_soup = BeautifulSoup(cerb_html, 'html.parser')
+    schia_soup = BeautifulSoup(schia_html, 'html.parser')
+    syrt_soup = BeautifulSoup(syrt_html, 'html.parser')
+
+    hemi_title = []
+    hemi_image_url = []
+
+
+    valles_name = valles_soup.find('h2', class_="title").text
+    valles_image = valles_soup.find('img', class_="wide-image")['src']
+
+    hemi_title.append(valles_name)
+    hemi_image_url.append(hemi_url+valles_image)
+
+    valles_browser.quit()
+
+    #identify and return the title and image
+    cerb_name = cerb_soup.find('h2', class_="title").text
+    cerb_image = cerb_soup.find('img', class_="wide-image")['src']
+
+    #append values to dictionary
+    hemi_title.append(cerb_name)
+    hemi_image_url.append(hemi_url+cerb_image)
+
+    cerb_browser.quit()
+
+    schia_name = schia_soup.find('h2', class_="title").text
+    schia_image = schia_soup.find('img', class_="wide-image")['src']
+
+    #append values to dictionary
+    hemi_title.append(schia_name)
+    hemi_image_url.append(hemi_url+schia_image)
+
+    schia_browser.quit()
+
+    syrt_name = syrt_soup.find('h2', class_="title").text
+    syrt_image = syrt_soup.find('img', class_="wide-image")['src']
+
+    #append values to dictionary
+    hemi_title.append(syrt_name)
+    hemi_image_url.append(hemi_url+syrt_image)
+
+    syrt_browser.quit()
+
+    mars_scrape['hemi_title'] = hemi_title
+    mars_scrape['hemi_image_url'] = hemi_image_url
 
     return mars_scrape
