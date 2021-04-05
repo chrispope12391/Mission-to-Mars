38dc61d97520a66b868ae30fe442f65f1044b18f
chrispope12391 <popex107@umn.edu>
2021-04-05 17:27:52 -0500

    22

--- Mission_to_Mars/scrape_mars.py
+++ Mission_to_Mars/scrape_mars.py
@@ -21,19 +21,18 @@ def scrape():
     # Parse HTML with Beautiful Soup
     soup = BeautifulSoup(html, 'html.parser')
 
-    # Retrieve all elements that contain mars article information
-    # mars_title = soup.find_all('div', class_='content_title')
-    # mars_teaser = soup.find('div', class_='article_teaser_body').text
-
+    # The dictionary where we will store our scraped information
     mars_scrape = {}
 
+    # scraping the title and article teaser and storing in the mars scrape dictionary
     mars_scrape['title'] = soup.find('div', class_='content_title').text
     mars_scrape['teaser'] = soup.find('div', class_='article_teaser_body').text
     browser.quit()
     
-    # This will pull the featured image
+    # Opens browser to access page
     new_browser = Browser('chrome', **executable_path, headless=True)
 
+    # storing mars image URL
     image_url = "https://spaceimages-mars.com/"
     new_browser.visit(image_url)
 
@@ -42,15 +41,18 @@ def scrape():
     # Parse HTML with Beautiful Soup
     new_soup = BeautifulSoup(new_html, 'html.parser')
 
-    # Retrieve all elements that contain header image information
+    # Retrieve and storeall elements that contain header image information
     image_results = new_soup.find('img', class_='headerimage fade-in')['src']
 
+    # storing image_rsults in mars_scrape
     mars_scrape['featured_img_url'] = image_url+image_results
 
     new_browser.quit()
 
+    #storing facts table URL
     facts_url = "https://galaxyfacts-mars.com/"
 
+    #read html to variable facts_df and then creating a dataframe to match example
     tables = pd.read_html(facts_url)
 
     facts_df = tables[0]
@@ -67,8 +69,10 @@ def scrape():
 
     facts_html = facts_df.to_html(border = 2)
 
+    #scrape html table to mars_scraoe
     mars_scrape['facts_df'] = facts_html
 
+    #storing hemisphere urls for each image
     hemi_url = "https://marshemispheres.com/"
     valles_url = hemi_url +"valles.html"
     cerbrus_url = hemi_url +"cerberus.html"
@@ -99,41 +103,45 @@ def scrape():
     schia_soup = BeautifulSoup(schia_html, 'html.parser')
     syrt_soup = BeautifulSoup(syrt_html, 'html.parser')
 
+    #create list to store title & image
     hemi_title = []
     hemi_image_url = []
 
-
+    #locating title and image for all hemispheres and storing in variables
     valles_name = valles_soup.find('h2', class_="title").text
     valles_image = valles_soup.find('img', class_="wide-image")['src']
 
+    #append values to list
     hemi_title.append(valles_name)
     hemi_image_url.append(hemi_url+valles_image)
 
     valles_browser.quit()
 
-    #identify and return the title and image
+    #locating title and image for all hemispheres and storing in variables
     cerb_name = cerb_soup.find('h2', class_="title").text
     cerb_image = cerb_soup.find('img', class_="wide-image")['src']
 
-    #append values to dictionary
+    #append values to list
     hemi_title.append(cerb_name)
     hemi_image_url.append(hemi_url+cerb_image)
 
     cerb_browser.quit()
 
+    #locating title and image for all hemispheres and storing in variables
     schia_name = schia_soup.find('h2', class_="title").text
     schia_image = schia_soup.find('img', class_="wide-image")['src']
 
-    #append values to dictionary
+    #append values to list
     hemi_title.append(schia_name)
     hemi_image_url.append(hemi_url+schia_image)
 
     schia_browser.quit()
 
+    #locating title and image for all hemispheres and storing in variables
     syrt_name = syrt_soup.find('h2', class_="title").text
     syrt_image = syrt_soup.find('img', class_="wide-image")['src']
 
-    #append values to dictionary
+    #append values to list
     hemi_title.append(syrt_name)
     hemi_image_url.append(hemi_url+syrt_image)
 
@@ -146,7 +154,7 @@ def scrape():
     {"title": hemi_title[3], "img_url": hemi_image_url[3]}
     ]
 
-
+    #store dictionary with list for each hemisphere and scrape to mars_scrape
     mars_scrape['hemi_image_title'] = hemisphere_image_urls
 
     return mars_scrape
